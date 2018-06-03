e094627a1ede08511ff988af05541f7a56c73220
Mickachu007 <38781773+Mickachu007@users.noreply.github.com>
2018-06-03 22:52:13 +0200

    Update ContratMaintenance.java
    Finissions de la classe ContratMaintenance

--- CashCash/src/javafxapplication1/ContratMaintenance.java
+++ CashCash/src/javafxapplication1/ContratMaintenance.java
@@ -1,15 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafxapplication1;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -21,8 +19,9 @@ public class ContratMaintenance {
     private Date     dateEcheance;
     private Date     dateRenouvel;
     private int      idClient;
+    private ArrayList<Materiel> lesMaterielsAssures;
     
-    private List <Materiel> lesMaterielsAssures = new ArrayList <Materiel>();
+//    private ArrayList <Materiel> lesMaterielsAssures = new ArrayList <Materiel>();
     
     public ContratMaintenance(){
         
@@ -62,4 +61,39 @@ public class ContratMaintenance {
     public void setIdClient(int idClient){
         this.idClient = idClient;
     }
+    
+    // AJOUTS MICKA
+    
+    public int getJourRestants() {
+        Date currentDate = new Date();
+        long diff = dateEcheance.getTime() - currentDate.getTime();
+        return (int)TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean estValide() {
+        Date currentDate = new Date();
+        return currentDate.after(dateDebut) && currentDate.before(dateEcheance);
+    }
+
+    public void ajouterMateriel(Materiel unMateriel) {
+        if (!dateDebut.before(unMateriel.getDateInstallation())) {
+            System.out.println("La date d'installation du matériel doit être supérieure à la date de début du contrat!");
+            return;
+        }
+        this.lesMaterielsAssures.add(unMateriel);
+    }
+
+    public ArrayList<Materiel> getLesMaterielsAssures() {
+        return lesMaterielsAssures;
+    }
+
+    /*public void loadMaterials() throws SQLException {
+        Connecting connection = new Connecting();
+        
+        if (connection == null) return;
+        
+        lesMaterielsAssures = (ArrayList<Materiel>) connection.chargerDepuisBase(String.valueOf(this.numContrat), "Materiel");
+        
+        System.out.println("Matériels récupérés : \u001B[36m" + this.lesMaterielsAssures.size() + "\u001B[0m.");
+    }*/
 }
